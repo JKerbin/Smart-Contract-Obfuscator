aa665b6fee973f34a789c8db1b1134282adbafa8
Jkerbin <2964535636@qq.com>
2024-11-24 22:23:04 +0800

    Improve obfuscattion function

--- src/main/utils/obfuscatior.ts
+++ src/main/utils/obfuscatior.ts
@@ -26,25 +26,31 @@ export const obfuscateTargetProject = async (mainWindow, directoryPath, projectN
             mainWindow.webContents.send('terminal-output', data.toString());
         });
 
-        terminalProcess = spawn('bin\\solc\\solc-windows.exe', ['--ast-compact-json', '-o', targetProject, `${targetProject}\\output-code.sol`], {
-            shell: true, // Enable shell commands
-        });
+        setTimeout(() => {
+            mainWindow.webContents.send('terminal-output', "\nEvent: Generate AST of output code");
+        }, 1000);
 
-        // Capture standard output
-        terminalProcess.stdout.on('data', (data: Buffer) => {
-            mainWindow.webContents.send('terminal-output', data.toString());
-        });
+        setTimeout(() => {
+            terminalProcess = spawn('bin\\solc\\solc-windows.exe', ['--ast-compact-json', '-o', targetProject, `${targetProject}\\output-code.sol`], {
+                shell: true, // Enable shell commands
+            });
 
-        // Capture standard error
-        terminalProcess.stderr.on('data', (data: Buffer) => {
-            mainWindow.webContents.send('terminal-output', data.toString());
-        });
+            // Capture standard output
+            terminalProcess.stdout.on('data', (data: Buffer) => {
+                mainWindow.webContents.send('terminal-output', data.toString());
+            });
 
-        // Listen for process close
-        terminalProcess.on('close', (code: number) => {
-            mainWindow.webContents.send('terminal-output', `\nTerminal exited with code ${code}`);
-            terminalProcess = null; // Reset the process variable
-        });
+            // Capture standard error
+            terminalProcess.stderr.on('data', (data: Buffer) => {
+                mainWindow.webContents.send('terminal-output', data.toString());
+            });
+
+            // Listen for process close
+            terminalProcess.on('close', (code: number) => {
+                mainWindow.webContents.send('terminal-output', `\nTerminal exited with code ${code}`);
+                terminalProcess = null; // Reset the process variable
+            });
+        }, 2000);
     } catch (error) {
         mainWindow.webContents.send('terminal-output', error);
     }
